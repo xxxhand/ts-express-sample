97190f01f6c55b2b7d431ba5494d0ffdbf285672
xxxhand <nohandboy@gmail.com>
2021-06-23 10:28:00 +0800

    Modify router

---
+++ packages/backend/src/application/workflows/v1-router.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction, Router } from 'express';
+import { CustomResult } from '@demo/app-common';
+import { ClientAuthController } from './client-auth-controller';
+
+export class V1Router {
+	public prefix: string = '/api/v1';
+	public router: Router = Router();
+
+	constructor() {
+		this._init();
+	}
+	
+	private _init = (): void => {
+		this.router
+			.all('/', async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+				res.locals['result'] = new CustomResult<string>().withResult('Hello world');
+				await next();
+			});
+		this.router
+			.use(ClientAuthController.build());
+	}
+}
--- packages/backend/src/bootstrap/app.ts
+++ packages/backend/src/bootstrap/app.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import { TNullable } from '@demo/app-common';
 import { AppInterceptor } from './app-interceptor';
 import * as appTracer from './app-request-tracer';
-import v1Route from '../application/workflows/v1-route';
+import {V1Router } from '../application/workflows/v1-router';
 
 const _PUBLIC_PATH = '../../../../public';
 
@@ -32,7 +32,10 @@ export class App {
 		this._app.use(express.urlencoded({ extended: false }));
 		this._app.use(appTracer.handle());
 		this._app.use(AppInterceptor.beforeHandler);
-		this._app.use('/api/v1', v1Route);
+		
+		const v1Router = new V1Router();
+		this._app.use(v1Router.prefix, v1Router.router);
+
 		this._app.use(AppInterceptor.completeHandler);
 		this._app.use(AppInterceptor.notFoundHandler);
 		this._app.use(AppInterceptor.errorHandler);
